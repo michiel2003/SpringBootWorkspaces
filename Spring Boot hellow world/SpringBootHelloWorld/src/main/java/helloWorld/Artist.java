11e8881af23219ff8ebec50770889d1e14afec3a
michiel2003 <michieldecap@gmail.com>
2021-01-08 09:38:54 +0100

    first commit

---
+++ Spring Boot hellow world/SpringBootHelloWorld/src/main/java/helloWorld/Artist.java
@@ -0,0 +1,33 @@
+package helloWorld;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Artist {
+	
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	private String Name;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	
+	public void setId(Integer Id) {
+		this.id = Id;
+	}
+	
+	public void setName(String name) {
+		this.Name = name;
+	}
+}
